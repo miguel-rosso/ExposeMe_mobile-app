6ea7f8cc8cec2bfd9c8d0e339df50c6f0d8ed223
miguel-rosso <miguelrosso60@gmail.com>
2026-04-03 20:16:57 +0200

    [CONFIG] Update server URL to new Render deployment

--- Client/config.ts
+++ Client/config.ts
@@ -1,7 +1,7 @@
 // Replace SERVER_URL with your server URL or local IP (http://(IP):3000/) for development
 const getEnvVars = () => {
   return {
-    SERVER_URL: "https://exposeme-mobile-app.onrender.com"
+    SERVER_URL: "https://exposeme-mobile-app-f72q.onrender.com"
   };
 };
 
